e6cabeb5b1d85d624c2943e6fe76cf8315f5e9d8
zeek <liweibooooo@126.com>
2019-08-11 12:04:16 +0800

    程序假死状态分析

--- src/main/java/com/wangwenjun/concurrency/chapter9/ProduceConsumerVersion2.java
+++ src/main/java/com/wangwenjun/concurrency/chapter9/ProduceConsumerVersion2.java
@@ -3,9 +3,13 @@ package com.wangwenjun.concurrency.chapter9;
 import java.util.stream.Stream;
 
 /***************************************
- * @author:Alex Wang
- * @Date:2017/2/19 QQ:532500648
- * QQ交流群:286081824
+ * 程序会一直处于假死的状态（但不是死锁）的情形举例如下：
+ * 在t1时刻，p1进行了正常的生产，完成其第一次循环
+ * 在t2时刻，p2由于仓库不为空，p2进行wait
+ * 在t3时刻，p1进行第二次循环，由于仓库不为空，p1进行wait
+ * 在t4时刻，c1进行消费，消费完了后，通知p1进行生产（p1进行生产，p1生成完了后，p1进行wait），c1进行wait
+ * 在t5时刻，c2进行消费，仓库不为空，c2进行消费，完了notify了c1（这是出错的点，notify不能指定notify的对象），然后c2wait，虽然notify了c1，但是c1一看仓库没有东西，自己把自己wait掉
+ * 在t6时刻，p1，p2，c1，c2均处于wait的状态，程序停住不执行了
  ***************************************/
 public class ProduceConsumerVersion2 {
 
