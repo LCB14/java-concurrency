33306685493efb13c6b51002b3fed36d7e551c78
zeek <liweibooooo@126.com>
2019-08-11 10:33:15 +0800

    增加有问题的生产者消费者程序

---
+++ src/main/java/com/wangwenjun/concurrency/chapter9/ProduceConsumerVersion21.java
@@ -0,0 +1,75 @@
+package com.wangwenjun.concurrency.chapter9;
+
+import java.util.stream.Stream;
+
+/***************************************
+ * 下面程序单个生产者，单个消费者的情况下是正常的
+ * 若是
+ * 1. 多个生产者
+ * 2. 多个消费者
+ * 3. 多个生产者多个消费者
+ * 都会出现问题
+ ***************************************/
+public class ProduceConsumerVersion21 {
+
+    private int i = 0;
+
+    final private Object LOCK = new Object();
+
+    private volatile boolean isProduced = false;
+
+    public void produce() {
+        synchronized (LOCK) {
+            if (isProduced) {
+                try {
+                    //多个生成的情况下，多个生产者会在此进行wait
+                    LOCK.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                i++;
+                System.out.println("P->" + i);
+                LOCK.notify();
+                isProduced = true;
+            }
+        }
+    }
+
+    public void consume() {
+        synchronized (LOCK) {
+            if (isProduced) {
+                System.out.println("C->" + i);
+                LOCK.notify();
+                isProduced = false;
+            } else {
+                try {
+                    LOCK.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ProduceConsumerVersion21 pc = new ProduceConsumerVersion21();
+        new Thread("t1"){
+            @Override
+            public void run() {
+                while (true) {
+                    pc.produce();
+                }
+            }
+        }.start();
+
+        new Thread("t2"){
+            @Override
+            public void run() {
+                while (true) {
+                    pc.consume();
+                }
+            }
+        }.start();
+    }
+}
