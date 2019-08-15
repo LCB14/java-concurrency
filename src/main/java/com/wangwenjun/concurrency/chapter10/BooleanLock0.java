094cd2d5d4b47ffbb17dcb62c9a372acd234aa1b
李伟博 <zeek_beijing@126.com>
2019-08-15 23:35:51 +0800

    初始版本的自定义锁

---
+++ src/main/java/com/wangwenjun/concurrency/chapter10/BooleanLock0.java
@@ -0,0 +1,60 @@
+package com.wangwenjun.concurrency.chapter10;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+/***************************************
+ * @author:Alex Wang
+ * @Date:2017/2/22 QQ:532500648
+ * QQ交流群:286081824
+ ***************************************/
+public class BooleanLock0 implements Lock {
+
+    //The initValue is true indicated the lock have be get.
+    //The initValue is false indicated the lock is free (other thread can get this.)
+    private boolean initValue;
+
+    private Collection<Thread> blockedThreadCollection = new ArrayList<>();
+
+    private Thread currentThread;
+
+    public BooleanLock0() {
+        this.initValue = false;
+    }
+
+    @Override
+    public synchronized void lock() throws InterruptedException {
+        while (initValue) {
+            blockedThreadCollection.add(Thread.currentThread());
+            this.wait();
+        }
+
+        blockedThreadCollection.remove(Thread.currentThread());
+        this.initValue = true;
+    }
+
+    @Override
+    public synchronized void lock(long mills) throws InterruptedException, TimeOutException {
+        
+    }
+
+    @Override
+    public synchronized void unlock() {
+        initValue = false;
+        Optional.of(Thread.currentThread().getName() + " release the lock monitor.")
+                    .ifPresent(System.out::println);
+        this.notifyAll();
+    }
+
+    @Override
+    public Collection<Thread> getBlockedThread() {
+        return Collections.unmodifiableCollection(blockedThreadCollection);
+    }
+
+    @Override
+    public int getBlockedSize() {
+        return blockedThreadCollection.size();
+    }
+}
