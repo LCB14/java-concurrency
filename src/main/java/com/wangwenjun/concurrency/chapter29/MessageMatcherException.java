894c12b716df2d11df5d60220ac425b86f503ef0
changbaol <1642272412@qq.com>
2021-08-07 17:59:11 +0800

    同步chapter13之后的源码

---
+++ src/main/java/com/wangwenjun/concurrency/chapter29/MessageMatcherException.java
@@ -0,0 +1,7 @@
+package com.wangwenjun.concurrency.chapter29;
+
+public class MessageMatcherException extends RuntimeException {
+    public MessageMatcherException(String message) {
+        super(message);
+    }
+}
